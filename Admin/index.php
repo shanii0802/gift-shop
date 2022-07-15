c9036af70d5732ce2150a04d9e819a12dc06366c
ZeeshanTest <zeeshancs619@gmail.com>
2022-07-15 15:27:47 +0500

    Updated Index file

--- Admin/index.php
+++ Admin/index.php
@@ -1,31 +1,70 @@
-<html>
-	<head>
-		<link rel="stylesheet" href="css/bootstrap.css"/>
-		<link rel="stylesheet" href="css/sty.css"/>
-		<style type="text/css">
-			body{
-				background: url('/bgg.jpg');
-			}
-		</style>
-	</head> 
-	<body>
-		<div class="container">
+<?php
+	if(isset($_POST["login"])){
+		$username= $_POST["username"];
+		$password= $_POST["password"];
+		//echo print_r($_POST);die();
+		if($username == "admin" AND $password == "admin"){
+			header("location: additem.php");
+		}
+		else {
+			echo "<script>alert('incorrect username or password')</script>";
+		}
 		
-		<div class="row">
-			<div class="col-12"> <img src="images/logow.jpg" height="200px" class="w-100" /></div>
-			
-		</div>
-		<div class="row cust-nav py-2">
-			<div class="col-md-12">
-				<?php include 'nav.php'; ?>
-			</div>
-		</div>
-		<div class="row">
-			<img src="images/banner.jpg" />
-		</div>
-		<h3><center>Thanks for visiting our store</center></h3>
-		</div>
+	}
 
 
-	</body>
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ONLINE FABRICS EMPORIUM</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+	  <style>
+        body{
+            background:#ecf0f5;
+        }
+		.nav-back{
+			background:#3c8dbc;
+			color: #FFF;
+		}
+	  </style>
+</head>
+<body>
+<nav class=" nav-back">
+            <center><h3>ONLINE FABRICS EMPORIUM</h3></center>
+            <center><h4>ADMIN PANNEL</h4></center>                     
+       </nav>
+	 <div class="container-fluid">
+
+	  	 <div class="container-fluid">
+            <div class="container mt-4">
+                <div class="row py-4">
+                    <div class="col-md-4" style="">
+                        <figure><img src="images/signin-image.jpg" alt="sing up image"></figure>
+                    </div>
+
+                    <div class="col-md-4">
+                        <h2 class="form-title">LOGIN</h2>
+                        <form method="POST">
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="username" id="username" required="_required"placeholder="username"/>
+                            </div>
+                            <div class="form-group">
+                                <input type="password" name="password" class="form-control" required="_required" placeholder="Password"/>
+                            </div>
+                            
+                            <div class="form-group form-button">
+                                <input type="submit" name="login" class="btn btn-warning btn-block" value="LOGIN"/>
+                            </div>
+            			     <div class="form-group">
+                                <a href="../" class="btn btn-success btn-block">GO TO USER PANNEL</a>
+                            </div>
+                        </form>
+                        
+                    </div>
+                </div>
+            </div>
+	  </div>
+	  </div>
+</body>
 </html>
