befa128278669fe9a656fa4e0daf88d1a2bd85c7
ZeeshanTest <zeeshancs619@gmail.com>
2022-07-15 15:20:13 +0500

    Added Index File

---
+++ Admin/index.php
@@ -0,0 +1,31 @@
+<html>
+	<head>
+		<link rel="stylesheet" href="css/bootstrap.css"/>
+		<link rel="stylesheet" href="css/sty.css"/>
+		<style type="text/css">
+			body{
+				background: url('/bgg.jpg');
+			}
+		</style>
+	</head> 
+	<body>
+		<div class="container">
+		
+		<div class="row">
+			<div class="col-12"> <img src="images/logow.jpg" height="200px" class="w-100" /></div>
+			
+		</div>
+		<div class="row cust-nav py-2">
+			<div class="col-md-12">
+				<?php include 'nav.php'; ?>
+			</div>
+		</div>
+		<div class="row">
+			<img src="images/banner.jpg" />
+		</div>
+		<h3><center>Thanks for visiting our store</center></h3>
+		</div>
+
+
+	</body>
+</html>
